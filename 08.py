b93b3745fde8ee15160f9fe7b716f3a447f0cc62
Emiya2098212383 <52161145+Emiya2098212383@users.noreply.github.com>
2019-06-30 12:18:13 +0800

    奇妙のlesson 0630

---
+++ 08.py
@@ -0,0 +1,10 @@
+# 输出指定范围内的整数
+# take input from the user
+print("-----华丽分割线-----")
+print("输出指定范围内所有整数：")
+lower = int(input("输入区间最小值: "))
+upper = int(input("输入区间最大值: "))
+for num in range(lower, upper + 1):
+    print(num)
+
+    
